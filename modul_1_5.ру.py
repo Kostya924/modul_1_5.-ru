df805825837221fe825cdf92e8f550b8af32c978
Kostya924 <podsekaev68@mail.ru>
2024-08-31 18:18:39 +0300

    Add files via upload

---
+++ modul_1_5.ру.py
@@ -0,0 +1,8 @@
+immutable_var_= 1, 2, 3, True, "string"
+print(immutable_var_)
+print(immutable_var_[1])
+print(immutable_var_)
+mutable_list = ['apple', 'orange', 'cocumber']
+print(mutable_list[1])
+mutable_list[0] = 'pear'
+print(mutable_list)
